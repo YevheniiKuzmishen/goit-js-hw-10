0979c7b17a1f2cd1783415a2c54f7b19695d0dbe
YevheniiKuzmishen <yevhenii.kuzmishen@gmail.com>
2024-05-10 09:15:43 +0200

    dependencies + library

---
+++ src/js/1-timer.js
@@ -0,0 +1,19 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+let userSelectedDate = 0;
